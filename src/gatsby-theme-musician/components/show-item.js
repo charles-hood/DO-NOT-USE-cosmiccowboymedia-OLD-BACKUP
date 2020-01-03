deda73eba479f0ec5c667b7a40597fdb00927390
Charles Hood <charles.hood@gmail.com>
2020-01-03 00:21:57 -0500

    Update show-item.js

--- src/gatsby-theme-musician/components/show-item.js
+++ src/gatsby-theme-musician/components/show-item.js
@@ -9,7 +9,7 @@ const ShowItem = ({ id, name, date, location, info_url, map_url }) => {
     <li key={id} className="GtmShowItem" sx={{ variant: "components.show" }}>
       <time
         dateTime={date}
-        aria-label={new Date(date).toLocaleDateString("en-GB", {
+        aria-label={new Date(date).toLocaleDateString("en-US", {
           day: "numeric",
           month: "long",
           year: "numeric",
@@ -22,7 +22,7 @@ const ShowItem = ({ id, name, date, location, info_url, map_url }) => {
           sx={{ variant: "components.show.date.month" }}
         >
           {" "}
-          {new Date(date).toLocaleDateString("en-GB", {
+          {new Date(date).toLocaleDateString("en-US", {
             month: "short",
           })}{" "}
         </span>
@@ -30,9 +30,9 @@ const ShowItem = ({ id, name, date, location, info_url, map_url }) => {
           className="GtmShowItem__day"
           sx={{ variant: "components.show.date.day" }}
         >
-          {new Date(date).toLocaleDateString("en-GB", {
+          {new Date(date).toLocaleDateString("en-US", {
             day: "numeric",
-          }-1)}
+          })}
         </span>
       </time>
       <div sx={{ variant: "components.show.colWrapper" }}>
